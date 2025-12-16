92565cc523a8daaa9f1dc5d390185a5940d437c2
ProBook <email@jewefy.com>
2025-12-16 12:28:44 +0300

    created two fields name, age and method hello and initialized two objects in the class Person using the constructor and added comments to describe how the constructor and and the hello method works.

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,24 @@
 public class Person {
-    String name;
-    int age;
-
-    Person () {
-        name = "Mohammed";
-        age = 24;
+    String name = "Mohammed";
+    int age =24;
+    /*
+    * this is the constructor which is a special type of method that initialize instance variables.
+    * by adding parameters to the constructor we can pass the values when creating the new object.*/
+    public Person (String Name , int Age) {
+        name = Name;
+        age = Age;
+    }
+    /*
+    * this is the hello method it has a return type of void so it doesn't return a value
+    * it prints out the sentence in the console
+    * it is called by the objects created from this class like this : person1.hello()*/
+    public void hello () {
+        System.out.println("Hello, my name is " + name + " and i am " + age + " years old");
     }
     public static void main(String[] args) {
-
+        Person person1 = new Person("Ahmed",22);
+        Person person2 = new Person("Omar",25);
+        person1.hello();
+        person2.hello();
     }
 }
