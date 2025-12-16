05a5980f8be4351b55e600d9888c8e8dca1e3336
ProBook <email@jewefy.com>
2025-12-16 12:10:21 +0300

    initial commit

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+    int age;
+
+    Person () {
+        name = "Mohammed";
+        age = 24;
+    }
+    public static void main(String[] args) {
+
+    }
+}
